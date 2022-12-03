68b0eb56d55c43977b390ae8fa75bf2d5d5c7bcc
Samir <emaildesamir@gmail.com>
2022-12-03 15:45:31 -0300

    Asteroids Basics

--- Assets/Scripts/Environment/Asteroids.cs
+++ Assets/Scripts/Environment/Asteroids.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Asteroids : MonoBehaviour
 {
+    [SerializeField] float maxSpeed;
+    [SerializeField] float maxTorque;
+
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector2 propulsion = new Vector2(Random.Range(-maxSpeed,maxSpeed), Random.Range(-maxSpeed, maxSpeed));
+        float torque = Random.Range(-maxTorque, maxTorque);
+
+        rb.AddForce(propulsion);
+        rb.AddTorque(torque);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
